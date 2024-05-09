93ec2d8e2d9e2dc0cfca76331f66d4cf6d55fcef
anwoomin <askcw39@naver.com>
2024-05-09 14:31:59 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,4 @@
 function App() {
-  // &&, //
-
-  // falsy(false 같은)
-  // : false, 0, "", null, undefined
-
-  const a = 0;
-  if (a) {
-    console.log("a는 참이다.");
-  } else {
-    console.log("a는 거짓이다.");
-  }
-
-  // truthy(true 같은) : falsy 가 아니면 truthy
-
-  let b = 0;
-
-  let c = b && "값이 있음";
-  console.log("c", c); // 0
-
-  let d = b || "값이 없음";
-  console.log("d", d);
-
   return <div></div>;
 }
 
