96d1d7e13a265d8ee5ef3a55ed819d7d6864232c
anwoomin <askcw39@naver.com>
2024-05-09 12:28:11 +0900

    필요없는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,5 @@
 function App() {
-  // {} 사용해서 javascript 코드 작성
-  const v1 = 5;
-  const v2 = 7;
-
-  function someAction() {
-    return "흥민";
-  }
-
-  function someAction2(p) {
-    return p * p;
-  }
-
-  return (
-    <>
-      <p>
-        {2} + {3} = {2 + 3}
-      </p>
-      <p>
-        {v1} + {v2} = {v1 + v2}
-      </p>
-      <p>
-        {v1} - {v2} = {v1 - v2}
-      </p>
-      <p>
-        {v1} X {v2} = {v1 * v2}
-      </p>
-      <p>{someAction()}</p>
-      <p>{someAction2(5)}</p>
-      <p>{v1 < 10 ? "작다" : "크다"}</p>
-      <p>{v1 > 3 ? "크다" : "작다"}</p>
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
