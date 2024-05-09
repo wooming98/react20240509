369195f549cdb1f44023e72ebfa0a915bb9044ca
anwoomin <askcw39@naver.com>
2024-05-09 10:22:43 +0900

    prettier 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-
 function App() {
-
   return (
     <>
       <div>
-          <h1>hello react</h1>
+        <h1>hello react</h1>
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
