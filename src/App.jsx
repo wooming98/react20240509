ed19e8fc765e22257b81b8d4145c2ef60cb549d2
anwoomin <askcw39@naver.com>
2024-05-09 14:42:29 +0900

    자바스크립트 데이터 타입

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,28 @@
 function App() {
+  // javascript review
+
+  // javascript data type
+  let a; // 값이 할당되지 않으면 undefined
+  console.log("a", typeof a);
+
+  let b = "문자열 타입"; // string
+  console.log("b", typeof b);
+
+  let c = 3; // number
+  console.log("c", typeof c);
+
+  let d = 3.14; // number
+  console.log("d", typeof d);
+
+  let e = true; // boolean
+  console.log("e", typeof e);
+
+  let f = {}; // object
+  console.log("f", typeof f);
+
+  let g = function () {};
+  console.log("g", typeof g);
+
   return <div></div>;
 }
 
