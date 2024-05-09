4926a9b4c71b38c63d0adb3063daedf97b91f9da
anwoomin <askcw39@naver.com>
2024-05-09 15:10:33 +0900

    javascript object와 property

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
 import React from "react";
 
 function App(props) {
-  // object : {}
-  // {} 안에 property명 : property값 쌍들이 작성됨
-  let a = {};
-  let b = { name: "흥민" };
-  let c = { address: "신촌", city: "서울", country: "한국" };
-
-  console.log("a", a);
-  console.log("b", b);
-  console.log("c", c);
-
-  console.log("b의 name", b.name);
-  console.log("c의 address", c.address);
-
-  console.log("b의 name", b["name"]);
-  console.log("c의 address", c["address"]);
-
-  b.name = "강인";
-  console.log("b의 name", b.name);
-
-  // 없는 프로퍼티에도 접근 가능
-  console.log("b의 city", b.city); // undefined
-  b.city = "부산"; // city 프로퍼티 추가됨
-  b.address = "강남"; // address 프로퍼티 추가됨
-
-  console.log("b", b);
-
-  return <div></div>;
+  const a = {
+    name: "흥민",
+    team: "토트넘",
+    salary: 500,
+  };
+  return (
+    <div>
+      {a.name}은 {a.team} 소속이고 연봉은 {a.salary}원 이다.
+    </div>
+  );
 }
 
 export default App;
