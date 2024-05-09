3e31bd1299cf1a72083afceb46d3b3b2a8efc7e7
anwoomin <askcw39@naver.com>
2024-05-09 14:43:34 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,6 @@
-function App() {
-  // javascript review
+import React from "react";
 
-  // javascript data type
-  let a; // 값이 할당되지 않으면 undefined
-  console.log("a", typeof a);
-
-  let b = "문자열 타입"; // string
-  console.log("b", typeof b);
-
-  let c = 3; // number
-  console.log("c", typeof c);
-
-  let d = 3.14; // number
-  console.log("d", typeof d);
-
-  let e = true; // boolean
-  console.log("e", typeof e);
-
-  let f = {}; // object
-  console.log("f", typeof f);
-
-  let g = function () {};
-  console.log("g", typeof g);
-
+function App(props) {
   return <div></div>;
 }
 
