9346756273d27c432e948511e7d7ecad94f68b4e
anwoomin <askcw39@naver.com>
2024-05-09 15:22:53 +0900

    피필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
 import React from "react";
 
 function App(props) {
-  const a = {
-    name: "흥민",
-    team: "토트넘",
-    salary: 500,
-  };
-  return (
-    <div>
-      {a.name}은 {a.team} 소속이고 연봉은 {a.salary}원 이다.
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
