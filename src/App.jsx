5a9c48a638d1f23e77e6a73cc99468f3ea232db7
anwoomin <askcw39@naver.com>
2024-05-09 15:38:20 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
 import React from "react";
 
 function App(props) {
-  const a = {};
-  console.log("a", typeof a);
-
-  const b = []; // 배열
-  console.log("b", typeof b);
-
-  // 배열 : 여러 값을 순서(index)대로 가지고 있는 객체
-
-  const c = ["son", "lee", "kim", "hwang"];
-  console.log("c", c);
-  console.log("c의 0", c[0]);
-  console.log("c의 1", c[1]);
-  console.log("c의 2", c[2]);
-  console.log("c의 3", c[3]);
-  console.log("c의 4", c[4]); // undefined
-
-  return (
-    <div>
-      <ul>
-        <li>{c[0]}</li>
-        <li>{c[1]}</li>
-        <li>{c[2]}</li>
-        <li>{c[3]}</li>
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
