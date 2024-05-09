8e42ab64c663504b5afb7a1fe88d91cc97972b65
anwoomin <askcw39@naver.com>
2024-05-09 14:30:04 +0900

    falsy, truthy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 function App() {
-  return null;
+  // &&, //
+
+  // falsy(false 같은)
+  // : false, 0, "", null, undefined
+
+  const a = 0;
+  if (a) {
+    console.log("a는 참이다.");
+  } else {
+    console.log("a는 거짓이다.");
+  }
+
+  // truthy(true 같은) : falsy 가 아니면 truthy
+
+  let b = 0;
+
+  let c = b && "값이 있음";
+  console.log("c", c); // 0
+
+  let d = b || "값이 없음";
+  console.log("d", d);
+
+  return <div></div>;
 }
 
 export default App;
