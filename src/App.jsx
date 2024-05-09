37b750c351315656c2b09acb3ae4afc574da0341
anwoomin <askcw39@naver.com>
2024-05-09 12:15:29 +0900

    jsx 내에서 {}로 자바스크립트 코드 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
 function App() {
-  return <></>;
+  // {} 사용해서 javascript 코드 작성
+  const v1 = 5;
+  const v2 = 7;
+
+  function someAction() {
+    return "흥민";
+  }
+
+  function someAction2(p) {
+    return p * p;
+  }
+
+  return (
+    <>
+      <p>
+        {2} + {3} = {2 + 3}
+      </p>
+      <p>
+        {v1} + {v2} = {v1 + v2}
+      </p>
+      <p>
+        {v1} - {v2} = {v1 - v2}
+      </p>
+      <p>
+        {v1} X {v2} = {v1 * v2}
+      </p>
+      <p>{someAction()}</p>
+      <p>{someAction2(5)}</p>
+      <p>{v1 < 10 ? "작다" : "크다"}</p>
+      <p>{v1 > 3 ? "크다" : "작다"}</p>
+    </>
+  );
 }
 
 export default App;
