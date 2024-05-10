fee8ee163e018e7f5f93723f2ab8582c42c67e33
anwoomin <askcw39@naver.com>
2024-05-10 16:22:04 +0900

    style prop 응용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
+function MyComp({ color }) {
+  return (
+    <div style={{ color: color }}>
+      {/*h1>lorem1^p>lorem3*/}
+      <h1>Lorem.</h1>
+      <p>Lorem ipsum dolor.</p>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      {/* red color */}
+      <MyComp color="red" />
+      {/* blue color */}
+      <MyComp color="blue" />
+    </div>
+  );
 }
 
 export default App;
