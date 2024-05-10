8055ba090576b17161b199aa37b5acc0958af265
anwoomin <askcw39@naver.com>
2024-05-10 09:23:36 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
 import React from "react";
 
 function App(props) {
-  // string
-  const s1 = "some string";
-  const s2 = "other string";
-  const s3 = "I'm 신뢰에요";
-  const s4 = '제 이름은 "흥민"입니다.';
-  // backtick
-  // template literal
-  const s5 = `another string`;
-
-  const name = "son";
-  const age = 30;
-
-  const s6 = name + " is " + age + " years.";
-  const s7 = `${name} is ${age} years.`;
-
-  return (
-    <div>
-      <p>{s6}</p>
-      <p>{s7}</p>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
