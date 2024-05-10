b0cc2a746013456509ef2fedde8dd48419156b33
anwoomin <askcw39@naver.com>
2024-05-10 16:22:20 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
 
-function MyComp({ color }) {
-  return (
-    <div style={{ color: color }}>
-      {/*h1>lorem1^p>lorem3*/}
-      <h1>Lorem.</h1>
-      <p>Lorem ipsum dolor.</p>
-    </div>
-  );
-}
-
 function App(props) {
-  return (
-    <div>
-      {/* red color */}
-      <MyComp color="red" />
-      {/* blue color */}
-      <MyComp color="blue" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
