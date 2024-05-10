4e613c8ec375715b7bc1d4e40aa6e11671b279e9
anwoomin <askcw39@naver.com>
2024-05-10 16:11:19 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
 import React from "react";
 
-function MyComp({ name, age, someProp, otherProp }) {
-  console.log("name", name);
-  console.log("age", age);
-  console.log("someProp", someProp); // true : 값없이 작성되면 true
-  console.log("otherProp", otherProp); // true
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComp name="son" age={30} someProp otherProp={true}></MyComp>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
