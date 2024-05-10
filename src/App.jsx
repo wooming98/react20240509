04c22bb9648d139365a9882e6bac832541f5ddf9
anwoomin <askcw39@naver.com>
2024-05-10 12:13:16 +0900

    얕은 복사, 깊은 복사, 깊은 복사를 얕은복사 여러번으로 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
 import React from "react";
 
 function App(props) {
+  const a = {
+    name: "John",
+    age: 44,
+    address: {
+      street: "Lorem Ipsum",
+      city: "seoul",
+    },
+  };
+
+  // 얕은 복사 (shallow copy)
+  const { ...b } = a;
+  b.age = 55;
+  console.log("b.age", b.age); // 55
+  console.log("a.age", a.age); // 44
+
+  b.address.city = "busan";
+  console.log("a.address.city", a.address.city); // busan
+  console.log("b.address.city", b.address.city); // busan
+
+  // 깊은 복사 (deep copy)
+  // 얕은 복사를 여러번 해서 해결
+  const { ...c } = a; // 얕은 복사
+  const { ...address1 } = a.address;
+  c.address = address1; // 깊은 복사
+  c.address.city = "london";
+  console.log("a.address.city", a.address.city); // busan
+  console.log("c.address.city", c.address.city); // london
+
   return <div></div>;
 }
 
