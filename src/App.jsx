9ac9d3504c676a110e0f0c7fcb920e9c03d602e6
anwoomin <askcw39@naver.com>
2024-05-10 14:14:56 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,7 @@
 import React from "react";
 
-// props : properties
-function MyButton(props) {
-  return (
-    <button
-      style={{
-        padding: "10px",
-        backgroundColor: "green",
-        color: "white",
-      }}
-    >
-      {props.name}
-    </button>
-  );
-}
-
 function App(props) {
-  return (
-    <div>
-      <button
-        style={{
-          padding: "10px",
-          backgroundColor: "blue",
-          color: "white",
-        }}
-      >
-        클릭1
-      </button>
-      <br />
-      <button
-        style={{
-          padding: "10px",
-          backgroundColor: "blue",
-          color: "white",
-        }}
-      >
-        클릭2
-      </button>
-      <br />
-      <MyButton name="클릭3" />
-      <br />
-      <MyButton name="클릭3" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
