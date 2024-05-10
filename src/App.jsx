88694c4239dc866c4de7bd01475d7b9397f316bd
anwoomin <askcw39@naver.com>
2024-05-10 11:40:29 +0900

    분해할당의 나머지 모두 문법으로 객체 복사

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,64 @@
 import React from "react";
 
 function App(props) {
+  // 나머지 모두 문법으로 객체 복사
+
+  const a = {
+    name: "son",
+    age: 33,
+    city: "london",
+  };
+  const b = a; // a와 b는 같은 객체 참조
+  b.age = 44;
+  console.log("b.age", b.age); // 44
+  console.log("a.age", a.age); // 44
+
+  const c = {
+    name: a.name,
+    age: a.age,
+    city: a.city,
+  };
+  c.age = 55;
+  console.log("c.age", c.age); // 55
+  console.log("a.age", a.age); // 44
+
+  // 객체의 각 프로퍼티를 복사
+  const { ...d } = a;
+  d.age = 66;
+  console.log("d.age", d.age); // 66
+  console.log("a.age", a.age); // 44
+
+  // 연습:
+  const e = {
+    name: "lee",
+    country: "korea",
+    team: "paris",
+    salary: 700,
+    city: "seoul",
+  };
+  // 코드 작성!
+  const { ...f } = e;
+
+  e.city = "busan";
+  // {
+  //   name: "lee",
+  //     country: "korea",
+  //     team: "paris",
+  //     salary: 700,
+  //     city: "busan",
+  // }
+  console.log(e);
+  /*
+  {
+    name: "lee",
+    country: "korea",
+    team: "paris",
+    salary: 700,
+    city: "seoul",
+  }
+   */
+  console.log(f);
+
   return <div></div>;
 }
 
