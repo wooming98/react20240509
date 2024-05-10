9998cdcd9f8ea48a77e99a55e3750b60ac430dc0
anwoomin <askcw39@naver.com>
2024-05-10 14:13:50 +0900

    props(properties)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
+// props : properties
+function MyButton(props) {
+  return (
+    <button
+      style={{
+        padding: "10px",
+        backgroundColor: "green",
+        color: "white",
+      }}
+    >
+      {props.name}
+    </button>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <button
+        style={{
+          padding: "10px",
+          backgroundColor: "blue",
+          color: "white",
+        }}
+      >
+        클릭1
+      </button>
+      <br />
+      <button
+        style={{
+          padding: "10px",
+          backgroundColor: "blue",
+          color: "white",
+        }}
+      >
+        클릭2
+      </button>
+      <br />
+      <MyButton name="클릭3" />
+      <br />
+      <MyButton name="클릭3" />
+    </div>
+  );
 }
 
 export default App;
