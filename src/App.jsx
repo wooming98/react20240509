9bc4f8758a8feeae3968a117204c59d88281b68f
anwoomin <askcw39@naver.com>
2024-05-10 16:22:41 +0900

    style props 응용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+function MyComp({ headColor, bodyColor }) {
+  return (
+    <div>
+      <h1 style={{ color: headColor }}>Lorem.</h1>
+      <p style={{ color: bodyColor }}>Lorem ipsum dolor.</p>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp headColor={"skyblue"} bodyColor={"gold"} />
+      <MyComp headColor={"gray"} bodyColor={"linen"} />
+    </div>
+  );
 }
 
 export default App;
