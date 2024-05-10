1b260f948bdb1828ab6aef5425eed862355e3a6c
anwoomin <askcw39@naver.com>
2024-05-10 15:11:28 +0900

    props의 값타입은 javascript 자료형 값은 {}안에 작성 단, string이면 {} 생략 가능

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
-function App(props) {
+function MyComp(props) {
   return <div></div>;
+}
+
+function App(props) {
+  return (
+    <div>
+      {/* props의 값타입은 javascript 값타입 */}
+      {/* {}에 값 입력, (string만 생략 가능) */}
+      <MyComp
+        name={"son"}
+        street="gangnam"
+        age={3.14}
+        address={{ city: "seoul", contry: "us" }}
+        foods={["pizza", "coffee"]}
+        married={true}
+        action={function () {
+          console.log("action");
+        }}
+        someAction={() => console.log("someAction")}
+      />
+    </div>
+  );
 }
 
 export default App;
