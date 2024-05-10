042a412e5cd4d5587c5092177f11e88d5c13b25f
anwoomin <askcw39@naver.com>
2024-05-10 16:50:22 +0900

    chakra ui 맛보기

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
 import React from "react";
-
-function MyComp({ headStyle, bodyStyle }) {
-  return (
-    <div>
-      <h1 style={headStyle}>Lorem.</h1>
-      <p style={bodyStyle}>Lorem ipsum dolor.</p>
-    </div>
-  );
-}
+import { Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
   return (
-    <div>
-      <MyComp
-        headStyle={{
-          padding: "5px",
-          border: "1px solid lightgray",
-          color: "blue",
-        }}
-        bodyStyle={{
-          margin: "5px",
-          border: "1px dotted skyblue",
-          color: "red",
-          textAlign: "center",
-        }}
-      />
-    </div>
+    <ChakraProvider>
+      <div>
+        <Button colorScheme={"blue"}> 클릭1 </Button>
+        <Button colorScheme={"orange"}> 클릭2 </Button>
+        <Button
+          sx={{
+            color: "skyblue",
+            fontSize: "30px",
+            padding: "20px",
+            margin: "20px",
+          }}
+          colorScheme={"gray"}
+        >
+          {" "}
+          클릭3{" "}
+        </Button>
+      </div>
+    </ChakraProvider>
   );
 }
 
