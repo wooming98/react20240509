887c2cdad08aba5c01377687240ffe32fede1159
anwoomin <askcw39@naver.com>
2024-05-10 14:38:37 +0900

    필요없는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
 
-function MyElem(props) {
-  console.log(props);
-  return <div></div>;
-}
-
-function MyComp({ city, country, price }) {
-  console.log("city", city);
-  console.log("country", country);
-  console.log("price", price);
-  return <div></div>;
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyElem name="son" age={33} />
-      <MyComp city="seoul" country="korea" price={500} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
