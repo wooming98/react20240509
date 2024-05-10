68613c28d0bdafea5d8e51f1548bcb7be2f95ec2
anwoomin <askcw39@naver.com>
2024-05-10 14:22:31 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,7 @@
 import React from "react";
 
-function MyComp(props) {
-  console.log(props);
-  return (
-    <div>
-      <p>name : {props.name}</p>
-      <p>age : props.age</p>
-      <p>address : {props.address}</p>
-      <p>city : {props.city}</p>
-      <p>country : {props.country}</p>
-      <p></p>
-    </div>
-  );
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComp name="son" age={33} address="seoul" />
-      <MyComp city="busan" country="USA" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
