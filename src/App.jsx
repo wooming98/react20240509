6fede53134bea35afe39386fed5eba760d02f522
anwoomin <askcw39@naver.com>
2024-05-10 14:29:05 +0900

    props를 분해할당으로 받기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
-function App(props) {
+function MyElem(props) {
+  console.log(props);
   return <div></div>;
+}
+
+function MyComp({ city, country, price }) {
+  console.log("city", city);
+  console.log("country", country);
+  console.log("price", price);
+  return <div></div>;
+}
+
+function App(props) {
+  return (
+    <div>
+      <MyElem name="son" age={33} />
+      <MyComp city="seoul" country="korea" price={500} />
+    </div>
+  );
 }
 
 export default App;
