a8c766e97e019d9ece460f6be07d2de94c00cdf7
anwoomin <askcw39@naver.com>
2024-05-13 12:28:00 +0900

    react는 re render 시점을 적절한 순간으로 선택함

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyComp1() {
+  const [val, setVal] = useState(0);
+
+  function updateVal() {
+    // react는 re render 시점을 적절한 순간으로 선택함
+    setVal(val + 1);
+    setVal(val + 1);
+    setVal(val + 1);
+    console.log("val", val);
+  }
+
+  return (
+    <div>
+      <div>{val}</div>
+      <div>
+        <button onClick={updateVal}>증가</button>
+      </div>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp1 />
+    </div>
+  );
 }
 
 export default App;
