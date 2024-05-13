24612d771f7686fef7a8cd8c7452f5c8a10f245f
anwoomin <askcw39@naver.com>
2024-05-13 11:03:21 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 import React from "react";
-import { Badge, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <div>
-        <div>
-          <Button colorScheme={"blue"}>클릭</Button>
-          <Button colorScheme={"green"}>
-            알림 <Badge>new</Badge>
-          </Button>
-        </div>
-      </div>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/MyValues.jsx
+++
@@ -1,8 +0,0 @@
-// export : 다른 파일에서 사용 가능하도록 내보내기
-export const value1 = "son";
-
-export const value2 = "lee";
-
-export const value3 = 4;
-
-export const value4 = { name: "kim", team: "뮌헨" };
