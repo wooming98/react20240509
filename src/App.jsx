670acb24c0a2b64c059277597a0c3de1f24a08e0
anwoomin <askcw39@naver.com>
2024-05-13 10:08:59 +0900

    as 로 별칭 주기 가능,
    여러 파일에 같은 변수가 있으면 as로 충돌을 피할 수 있음

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { value1, value2, value3, value4 } from "./MyValues.jsx";
+// import : 다른 파일에 있는 변수를 현재 파일에서 사용
+// 다른 파일의 같은 변수를 import 를 할 때 as로 다른 별칭을 줄 수 있음
+import { value1, value2, value3 as v3, value4 } from "./MyValues.jsx";
+import { myAddress, myStr, value3 } from "./MyComps.jsx";
 
 function App(props) {
   return (
@@ -7,7 +10,10 @@ function App(props) {
       <div>value1 : {value1}</div>
       <div>value2 : {value2}</div>
       <div>value3 : {value3}</div>
+      <div>v3 : {v3}</div>
       <div>value4 : {value4.team}</div>
+      <div>myStr : {myStr}</div>
+      <div>myAddress : {myAddress}</div>
     </div>
   );
 }
---
+++ src/MyComps.jsx
@@ -0,0 +1,5 @@
+export const myStr = "korea";
+
+export const myAddress = "seoul";
+
+export const value3 = 99;
