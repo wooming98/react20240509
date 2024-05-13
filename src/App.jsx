fdac9dd7234feca05023f8b9afd341ca3a83a484
anwoomin <askcw39@naver.com>
2024-05-13 10:27:48 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import { action1, action2, action3, MyComp as YourComp } from "./MyComps.jsx";
-import { HerComp } from "./HerComp.jsx";
 
 function App(props) {
-  action1();
-  action2();
-  action3();
-  return (
-    <div>
-      <YourComp />
-      <HerComp />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
