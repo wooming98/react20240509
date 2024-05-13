7a9aa6427fb40b4d6bfc7756a23f16d32d1c2648
anwoomin <askcw39@naver.com>
2024-05-13 16:44:02 +0900

    하위컴포넌트의 상태와 이벤트를 상위컴포넌트에서 관리할 수 있다. 이 때 props 사용해서 이벤트핸들러메소드와 상태를 전달해 줌.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyButton({ onClick }) {
+  return <button onClick={onClick}>클릭</button>;
+}
+
+function MyNumber({ number }) {
+  return <div>number : {number}</div>;
+}
 
 function App(props) {
-  return <div></div>;
+  const [number, setNumber] = useState(0);
+
+  function handleNumberChange() {
+    setNumber(number + 1);
+  }
+
+  // 하위컴포넌트의 상태와 이벤트를 상위컴포넌트에서 관리할 수 있다.
+  // 이 때 props 사용해서 이벤트핸들러메소드와 상태를 전달해 줌.
+  return (
+    <div>
+      <MyButton onClick={handleNumberChange} />
+      <MyNumber number={number} />
+    </div>
+  );
 }
 
 export default App;
