1165b1bfce6c3cc22d4408971a67b37ff96f5667
anwoomin <askcw39@naver.com>
2024-05-13 11:41:36 +0900

    상태가 변경될 때 컴포넌트가 다시 실행됨

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyComp() {
+  const [val1, setVal1] = useState(0);
+
+  let val2 = 0;
+
+  function updateVal1() {
+    return setVal1(val1 + 1);
+  }
+
+  function updateVal2() {
+    return val2++;
+  }
+
+  console.log("다시 호출됨");
+
+  return (
+    <div>
+      <button onClick={updateVal1}>val1 {val1}</button>
+      <button onClick={updateVal2}>val2 {val2}</button>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp />
+    </div>
+  );
 }
 
 export default App;
