b385240f65de942329b317570b72ea623282ac08
anwoomin <askcw39@naver.com>
2024-05-13 14:41:13 +0900

    상태예제3) input에 입력시 p에 출력

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  const [text, setText] = useState("");
+
+  function handleUpdateText(e) {
+    setText(e.target.value);
+  }
+
+  return (
+    <div>
+      <input type="text" onChange={handleUpdateText} />
+      <p>{text}</p>
+    </div>
+  );
 }
 
 export default App;
