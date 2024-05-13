d359c7a937cf22a81c532cd78a8f6fca40d28bcd
anwoomin <askcw39@naver.com>
2024-05-13 10:39:15 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-import { v1, v2 } from "./MyComps.jsx";
 
 function App(props) {
-  return (
-    <div>
-      <div>{v1}</div>
-      <div>{v2}</div>
-      <div>{v3}</div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
