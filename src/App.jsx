d1a639a966c5b9630651b359cfd3cab0c4c854f5
anwoomin <askcw39@naver.com>
2024-05-13 14:35:17 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import React, { useState } from "react";
-
-const foods = ["pizza", "raman", "coffee", "latte", "burger"];
+import React from "react";
 
 function App(props) {
-  const [index, setIndex] = useState(0);
-  const food = foods[index];
-
-  return (
-    <div>
-      {index > 0 && <button onClick={() => setIndex(index - 1)}>이전</button>}
-      {index < foods.length - 1 && (
-        <button onClick={() => setIndex(index + 1)}>다음</button>
-      )}
-      <div>{food}</div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
