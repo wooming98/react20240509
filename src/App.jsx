21091cced425a1fc34d685cc91edb78a40dfed56
anwoomin <askcw39@naver.com>
2024-05-13 14:20:56 +0900

    상태예제2) state 변경되면 다시 그려짐
    state를 꼭 출력하는 용도로 사용하지 않아도 됨
    
    가공해서 사용할 수 있음

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+
+const foods = ["pizza", "raman", "coffee", "latte", "burger"];
 
 function App(props) {
-  return <div></div>;
+  const [index, setIndex] = useState(0);
+  const food = foods[index];
+
+  return (
+    <div>
+      {index > 0 && <button onClick={() => setIndex(index - 1)}>이전</button>}
+      {index < foods.length - 1 && (
+        <button onClick={() => setIndex(index + 1)}>다음</button>
+      )}
+      <div>{food}</div>
+    </div>
+  );
 }
 
 export default App;
