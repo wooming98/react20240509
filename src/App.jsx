62dd6cad8aeb3423a49f4dc825e163eea442c95a
anwoomin <askcw39@naver.com>
2024-05-13 14:02:11 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
-import React, { useState } from "react";
-
-function MyComp() {
-  // 여러 상태가 있을 수 있음
-  const [val1, setVal1] = useState(0);
-  const [val2, setVal2] = useState(0);
-
-  console.log("다시 그려짐");
-
-  return (
-    <div>
-      <div>{val1}</div>
-      <div>
-        <button onClick={() => setVal1(val1 + 1)}>click1</button>
-      </div>
-      <div>{val2}</div>
-      <div>
-        <button onClick={() => setVal2(val2 + 1)}>click2</button>
-      </div>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <MyComp />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
