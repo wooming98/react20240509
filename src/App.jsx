afdb29eb1235f8759b1fb8a6b6eb78ce6584e575
anwoomin <askcw39@naver.com>
2024-05-13 15:36:17 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [text, setText] = useState("");
-  const [list, setList] = useState([]);
-
-  function handleClick() {
-    const [...nextList] = list;
-    nextList.push(text);
-    setList(nextList);
-    setText("");
-  }
-
-  return (
-    <div>
-      <input
-        value={text}
-        type="text"
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button onClick={handleClick}>추가</button>
-      <ul>
-        {list.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
