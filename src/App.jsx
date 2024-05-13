cc08a40bb789db32da08159fca975f0ee799e11e
anwoomin <askcw39@naver.com>
2024-05-13 11:44:02 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
-import React, { useState } from "react";
-
-function MyComp() {
-  const [val1, setVal1] = useState(0);
-
-  let val2 = 0;
-
-  function updateVal1() {
-    return setVal1(val1 + 1);
-  }
-
-  function updateVal2() {
-    return val2++;
-  }
-
-  console.log("다시 호출됨");
-
-  return (
-    <div>
-      <button onClick={updateVal1}>val1 {val1}</button>
-      <button onClick={updateVal2}>val2 {val2}</button>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <MyComp />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
