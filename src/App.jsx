846d1b507477e5ba0fb404f9393ee0269bd30b2f
anwoomin <askcw39@naver.com>
2024-05-13 10:32:29 +0900

    react component를 만드는 방법들
    이름 있는 함수
    이름 없는 함수
    arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { ArrowComp, MyComp, Youcomp } from "./MyComps.jsx";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp />
+      <Youcomp />
+      <ArrowComp />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/MyComps.jsx
@@ -0,0 +1,11 @@
+// react component 들
+
+export function MyComp() {
+  return <div>mycomp</div>;
+}
+
+export const Youcomp = function () {
+  return <div>yourcomp</div>;
+};
+
+export const ArrowComp = () => <div>arrow function component</div>;
