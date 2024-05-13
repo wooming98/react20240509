32900d13b800e2ae46589770f172da25264aeadc
anwoomin <askcw39@naver.com>
2024-05-13 09:35:49 +0900

    component 다른 파일로 옮기기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { MyComp } from "./MyComp.jsx";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/MyComp.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function MyComp() {
+  return <div>hello mycomp</div>;
+}
