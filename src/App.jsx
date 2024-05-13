730e7722e043bf62f6a5c8633daabd54ac2c1476
anwoomin <askcw39@naver.com>
2024-05-13 12:12:31 +0900

    상태가 객체면, 복사해서 새 객체를 만들어 써야함.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
 import React, { useState } from "react";
 
 function MyComp() {
-  const [val, setVal] = useState(0);
-  console.log("다시 그려짐");
+  const [val, setVal] = useState({ name: "son" });
+
+  function updateVal() {
+    // 상태가 바뀌면 다시 그림
+    // 새 객체를 만들어서 set
+    val.name = "lee";
+    setVal(val);
+  }
+
   return (
     <div>
-      <button onClick={() => setVal(0)}>클릭 {val}</button>
+      {val.name}
+      <button onClick={updateVal}>click</button>
     </div>
   );
-  return null;
+}
+
+function MyComp2() {
+  const [val, setVal] = useState({ name: "son" });
+  function updateVal() {
+    // 객체를 복사해서 새 객체를 만들어 써야함
+    const { ...newVal } = val; // 얕은복사
+    newVal.name = "lee";
+    setVal(newVal);
+  }
+  return (
+    <div>
+      {val.name}
+      <button onClick={updateVal}>변경</button>
+    </div>
+  );
 }
 
 function App(props) {
   return (
     <div>
       <MyComp />
+      <MyComp2 />
     </div>
   );
 }
