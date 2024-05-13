8ec2489b8cba39df3fdaccb84298cf9f1c1a131d
anwoomin <askcw39@naver.com>
2024-05-13 11:02:50 +0900

    chakra ui component 사용 맛보기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Badge, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <div>
+        <div>
+          <Button colorScheme={"blue"}>클릭</Button>
+          <Button colorScheme={"green"}>
+            알림 <Badge>new</Badge>
+          </Button>
+        </div>
+      </div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
--- src/MyComps.jsx
+++ src/MyComps.jsx
@@ -2,4 +2,5 @@ const v1 = "korea";
 const v2 = 44;
 export const v3 = "seoul";
 
+// 한 번에 export
 export { v1, v2 };
