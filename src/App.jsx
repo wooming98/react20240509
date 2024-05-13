26152b27556800cbdf4986e81bf58cb4a53e6161
anwoomin <askcw39@naver.com>
2024-05-13 12:29:45 +0900

    코드 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
-import React, { useState } from "react";
-
-function MyComp1() {
-  const [val, setVal] = useState(0);
-
-  function updateVal() {
-    // react는 re render 시점을 적절한 순간으로 선택함
-    setVal(val + 1);
-    setVal(val + 1);
-    setVal(val + 1);
-    console.log("val", val);
-  }
-
-  return (
-    <div>
-      <div>{val}</div>
-      <div>
-        <button onClick={updateVal}>증가</button>
-      </div>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <MyComp1 />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
