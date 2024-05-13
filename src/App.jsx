81fbc76d88195c4253e532a4cbccccdfb692d3a6
anwoomin <askcw39@naver.com>
2024-05-13 09:48:35 +0900

    import, export

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { value1, value2, value3, value4 } from "./MyValues.jsx";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <div>value1 : {value1}</div>
+      <div>value2 : {value2}</div>
+      <div>value3 : {value3}</div>
+      <div>value4 : {value4.team}</div>
+    </div>
+  );
 }
 
 export default App;
