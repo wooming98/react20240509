e86fbb1b2b8c3baacf8ce27ca25e4318919fd632
anwoomin <askcw39@naver.com>
2024-05-13 11:32:51 +0900

    react 컴포넌트 상태
    컴포넌트는 상태를 가지고 있음
    상태가 변경되면 다시 그려짐
    상태는 react가 관리함

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyComp() {
+  let val = 0;
+
+  function increment() {
+    val++;
+  }
+
+  return (
+    <div>
+      {val}
+      <button onClick={increment}>증가</button>
+    </div>
+  );
+}
+
+function MyComp2() {
+  // react component 는 상태(state)가 바뀌면 다시 그려짐(re render)
+  // 각 component의 state는 react가 관리
+
+  // useState 함수는 배열을 리턴함
+  // 첫번째 원소(0번 index) : 상태값
+  // 두번째 원소(2번 index) : 상태를 업데이트하는 함수
+  // useState의 파라미터로 초기값 전달
+  const [val, setVal] = useState(0);
+
+  function increment() {
+    setVal(val + 1);
+  }
+
+  return (
+    <div>
+      {val}
+      <button onClick={increment}>증가</button>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp />
+      <MyComp2 />
+    </div>
+  );
 }
 
 export default App;
