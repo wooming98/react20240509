89847d4a7f8b8d33260b1d9a509b31da7e44bc7d
anwoomin <askcw39@naver.com>
2024-05-13 15:08:03 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 
 function App(props) {
-  const [text, setText] = useState("");
-
-  function handleUpdateText(e) {
-    setText(e.target.value);
-  }
-
-  return (
-    <div>
-      <input type="text" onChange={handleUpdateText} />
-      <p>{text}</p>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
