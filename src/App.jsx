96acac22ea86cfff9a0112299f64ccf109f2f790
anwoomin <askcw39@naver.com>
2024-05-13 14:05:06 +0900

    상태예제1) 버튼 클릭하면 1씩 증가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyCounter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      {count}
+      <button onClick={() => setCount(count + 1)}>증가</button>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyCounter />
+    </div>
+  );
 }
 
 export default App;
