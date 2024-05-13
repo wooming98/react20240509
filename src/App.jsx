08c9e16fbafe89e83f6eddee7fb0a77ac00be08c
anwoomin <askcw39@naver.com>
2024-05-13 12:19:26 +0900

    코드 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,7 @@
-import React, { useState } from "react";
-
-function MyComp1() {
-  const [val, setVal] = useState([]);
-
-  function addItem() {
-    val.push("a");
-    setVal(val);
-  }
-
-  return (
-    <div>
-      <div>{val}</div>
-      <div>
-        <button onClick={addItem}>추가</button>{" "}
-      </div>
-    </div>
-  );
-}
-
-function MyComp2() {
-  const [val, setVal] = useState([]);
-  // 상태가 객체면 새 객체로 복사해서 써야함
-  function addItem() {
-    const [...newVal] = val;
-    newVal.push("a");
-    setVal(newVal);
-  }
-
-  return (
-    <div>
-      <div>{val}</div>
-      <div>
-        <button onClick={addItem}>추가</button>
-      </div>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <MyComp1 />
-      <MyComp2 />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
