012f76820cf50261a271c7ff3d6fe30703db927c
anwoomin <askcw39@naver.com>
2024-05-14 16:36:32 +0900

    고객 번호로 고객 이름 조회, proxy 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  const [customerId, setCustomerId] = useState("");
+  const [customerName, setCustomerName] = useState("");
+  function handleSearchClick() {
+    // 조회버튼 클릭시
+    // /api/customer?id=3 로 요청
+    axios
+      .get(`/api/customer?id=${customerId}`)
+      .then((response) => response.data)
+      .then((data) => setCustomerName(data));
+  }
+
+  return (
+    <div>
+      <div>
+        <input type="number" onChange={(e) => setCustomerId(e.target.value)} />
+        <button onClick={handleSearchClick}>조회</button>
+      </div>
+      <div>이름 : {customerName}</div>
+    </div>
+  );
 }
 
 export default App;
