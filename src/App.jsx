1928f1150f2a75d15ff1040a16f623821e7357a7
anwoomin <askcw39@naver.com>
2024-05-14 12:06:24 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,7 @@
-import React, { createContext, useContext, useState } from "react";
+import React from "react";
 
-// step1 : create the context
-const MessageContext = createContext("");
-
-function MyBox() {
-  // step2 : use the context
-  const message = useContext(MessageContext);
-
-  return <div>{message}</div>;
-}
-
-function MySection() {
-  return <MyBox />;
-}
-
-function MyContainer() {
-  return <MySection />;
-}
-
-// context
-// https://react.dev/learn/passing-data-deeply-with-context
 function App(props) {
-  const [message, setMessage] = useState("");
-
-  return (
-    <div>
-      <input type="text" onChange={(e) => setMessage(e.target.value)} />
-      <p>{message}</p>
-      <hr />
-      {/* step3 : provide the context */}
-      <MessageContext.Provider value={message}>
-        <MyContainer />
-      </MessageContext.Provider>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
