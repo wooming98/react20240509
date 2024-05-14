ec9c760120e5a0af41f572564e5df16159dc2e1c
anwoomin <askcw39@naver.com>
2024-05-14 11:26:25 +0900

    prop drilling 코드

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
+function MyBox({ message }) {
+  return <div>{message}</div>;
+}
+
+function MySection({ message }) {
+  return <MyBox message={message} />;
+}
+
+function MyContainer({ message }) {
+  return <MySection message={message} />;
+}
+
+// context
+// https://react.dev/learn/passing-data-deeply-with-context
 function App(props) {
-  return <div></div>;
+  const [message, setMessage] = useState("");
+
+  return (
+    <div>
+      <input type="text" onChange={(e) => setMessage(e.target.value)} />
+      <p>{message}</p>
+      <hr />
+      <MyContainer message={message} />
+    </div>
+  );
 }
 
 export default App;
