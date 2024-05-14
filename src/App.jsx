93c0096368652fc89356bf12f2f4f23a5dd75f77
anwoomin <askcw39@naver.com>
2024-05-14 12:25:53 +0900

    컨텍스트로 데이터(객체) 전달

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
-import React, { useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 
-function MyInput({ onChange, text }) {
+// step1 : 컨텍스트 만들기
+const TextContext = createContext(null);
+
+function MyInput() {
+  // step2 : 컨텍스트 사용하기
+  const textHandler = useContext(TextContext);
+
   return (
     <div>
-      <input type="text" onChange={(e) => onChange(e.target.value)} />
-      <p>{text}</p>
+      <input
+        type="text"
+        onChange={(e) => textHandler.updateText(e.target.value)}
+      />
+      <p>{textHandler.text}</p>
     </div>
   );
 }
 
-function MyText({ text }) {
+function MyText() {
+  const textHandler = useContext(TextContext);
   return (
     <div>
-      <p>{text}</p>
+      <p>{textHandler.text}</p>
     </div>
   );
 }
@@ -26,8 +36,11 @@ function App(props) {
 
   return (
     <div>
-      <MyInput text={text} onChange={handleUpdateText} />
-      <MyText text={text} />
+      {/* step3 : 컨텍스트 제공하기 */}
+      <TextContext.Provider value={{ text, updateText: handleUpdateText }}>
+        <MyInput />
+        <MyText />
+      </TextContext.Provider>
     </div>
   );
 }
