d93951889524eeb0b50c612c2b4a40f58747a1c3
anwoomin <askcw39@naver.com>
2024-05-14 11:16:36 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,7 @@
-import React, { useState } from "react";
-
-function MyButton({ count, onClick }) {
-  return (
-    <div>
-      <button onClick={onClick}>UP {count}</button>
-    </div>
-  );
-}
-
-function MyBox({ count }) {
-  return <div>{count}번 클릭됨</div>;
-}
+import React from "react";
 
 function App(props) {
-  const [count, setCount] = useState(0);
-
-  function handleUpdateCount() {
-    setCount(count + 1);
-  }
-
-  return (
-    <div>
-      <MyButton count={count} onClick={handleUpdateCount} />
-
-      <hr />
-
-      <MyBox count={count} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
