5cc526efc6ee3e71039b93e092c8dcda926d03f7
anwoomin <askcw39@naver.com>
2024-05-14 12:18:53 +0900

    prop 전달

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyInput({ onChange, text }) {
+  return (
+    <div>
+      <input type="text" onChange={(e) => onChange(e.target.value)} />
+      <p>{text}</p>
+    </div>
+  );
+}
+
+function MyText({ text }) {
+  return (
+    <div>
+      <p>{text}</p>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  const [text, setText] = useState("");
+
+  function handleUpdateText(t) {
+    setText(t);
+  }
+
+  return (
+    <div>
+      <MyInput text={text} onChange={handleUpdateText} />
+      <MyText text={text} />
+    </div>
+  );
 }
 
 export default App;
