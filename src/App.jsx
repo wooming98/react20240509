692b3b80721475ad393e67d9715276d4347d7a1c
anwoomin <askcw39@naver.com>
2024-05-16 14:06:58 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,7 @@
 import React from "react";
-import axios from "axios";
 
 function App(props) {
-  // json 데이터 타입
-  // 문자열, 수, 불리언, 객체, 배열, null
-
-  function handleClick1() {
-    const data = {
-      name: "son",
-      age: 33,
-      married: false,
-      team: {
-        location: "london",
-        name: "토트넘",
-      },
-      item: ["phone", "shoes", "shirts"],
-      address: null,
-    };
-    axios.post("/api/main41/sub5", data);
-  }
-
-  function handleClick2() {
-    axios.post("/api/main41/sub6", {
-      name: "son",
-      age: 33,
-    });
-  }
-
-  return (
-    <div>
-      <button onClick={handleClick1}>json 데이터와 요청</button>
-      <br />
-      <button onClick={handleClick2}>json 데이터와 요청</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
