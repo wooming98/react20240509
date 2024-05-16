d0cd79a7258f8be6cbdc991bbf96edc3ba6ca537
anwoomin <askcw39@naver.com>
2024-05-16 14:46:43 +0900

    JSON 응답 파싱 하기

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,33 @@ function App(props) {
   }
 
   function handleClick3() {
-    axios.get("/api/main42/sub3").then((param) => console.log(param));
+    axios.get("/api/main42/sub3").then((res) => console.log(res.data));
+  }
+
+  function handleClick4() {
+    axios.get("/api/main42/sub4").then((res) => console.log(res.data));
+  }
+
+  function handleClick5() {
+    axios.get("/api/main42/sub5").then((res) => console.log(res.data));
   }
 
   return (
     <div>
       <button onClick={handleClick1}>응답 받기</button>
       <br />
-      {/*  버튼이 클릭되면 /api/main42/sub2 get 요청 후 응답본문을 콘솔에 출력*/}
+      {/* 버튼이 클릭되면 /api/main42/sub2 get 요청 후
+      응답본문을 콘솔에 출력 */}
       <button onClick={handleClick2}>응답 받기</button>
       <br />
       <p>{result}</p>
-      <br />
       <button onClick={handleClick3}>JSON 응답 받기</button>
+      <br />
+      {/* /api/main42/sub4 get 요청 후
+       응답 받은 내용을 콘솔에 출력 */}
+      <button onClick={handleClick4}>JSON 응답 받기</button>
+      <br />
+      <button onClick={handleClick5}>JSON 응답 받기</button>
     </div>
   );
 }
