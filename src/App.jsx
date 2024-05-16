01323613f7f21bebca234d2e4cc3e8bcc9bc82f9
anwoomin <askcw39@naver.com>
2024-05-16 10:17:10 +0900

    /api/someurl3의 get 요청 버튼 작성

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,31 @@ function App(props) {
     axios.post("/api/someurl");
   }
 
+  function handleClickButton3() {
+    axios.get("/api/someurl2");
+  }
+
+  function handleClickButton4() {
+    axios.post("/api/someurl2");
+  }
+
+  function handleClickButton5() {
+    const params = new URLSearchParams();
+    params.append("id", 3);
+    params.append("name", "son");
+    params.append("email", "son@son.com");
+    // axios.get("/api/someurl3?" + params);
+    axios.get(`/api/someurl3?${params}`);
+  }
+
   return (
     <div>
       <button onClick={handleClickButton1}>get 요청</button>
       <button onClick={handleClickButton2}>post 요청</button>
+      {/* /api/someurl2 get/post */}
+      <button onClick={handleClickButton3}>get 요청2</button>
+      <button onClick={handleClickButton4}>post 요청2</button>
+      <button onClick={handleClickButton5}>get 요청 with query string</button>
     </div>
   );
 }
