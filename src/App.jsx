b701c9df1ae28131102aa0a7b25040a5c9c56fed
anwoomin <askcw39@naver.com>
2024-05-16 11:01:59 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,7 @@
 import React from "react";
-import axios from "axios";
 
 function App(props) {
-  function handleClickButton1() {
-    // ajax get 요청
-    axios.get("/api/someurl");
-  }
-
-  function handleClickButton2() {
-    // ajax post 요청
-    axios.post("/api/someurl");
-  }
-
-  function handleClickButton3() {
-    axios.get("/api/someurl2");
-  }
-
-  function handleClickButton4() {
-    axios.post("/api/someurl2");
-  }
-
-  function handleClickButton5() {
-    const params = new URLSearchParams();
-    params.append("id", 3);
-    params.append("name", "son");
-    params.append("email", "son@son.com");
-    // axios.get("/api/someurl3?" + params);
-    axios.get(`/api/someurl3?${params}`);
-  }
-
-  function handleClickButton6() {
-    const params = new URLSearchParams();
-    params.append("id", 5);
-    params.append("city", "서울");
-    params.append("country", "한국");
-
-    axios.post("/api/someurl3", params);
-  }
-
-  return (
-    <div>
-      <button onClick={handleClickButton1}>get 요청</button>
-      <button onClick={handleClickButton2}>post 요청</button>
-      {/* /api/someurl2 get/post */}
-      <button onClick={handleClickButton3}>get 요청2</button>
-      <button onClick={handleClickButton4}>post 요청2</button>
-      {/* data도 함께 보내기 */}
-      <button onClick={handleClickButton5}>get 요청 with query string</button>
-      <button onClick={handleClickButton6}>post 요청 with data</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
