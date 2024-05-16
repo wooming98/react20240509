099e37ea61c8407f6d3d8d6ca0e35c8788c7a49e
anwoomin <askcw39@naver.com>
2024-05-16 15:40:59 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import axios from "axios";
 
 function App(props) {
-  return (
-    <div>
-      <button onClick={() => axios.get("/api/main43/sub1")}>200 응답</button>
-      <button onClick={() => axios.get("/api/main43/sub2")}>400 응답</button>
-      <button onClick={() => axios.get("/api/main43/sub3")}>401 응답</button>
-      <button onClick={() => axios.get("/api/main43/sub4")}>403 응답</button>
-      <button onClick={() => axios.get("/api/main43/sub5")}>404 응답</button>
-      <button onClick={() => axios.get("/api/main43/sub6")}>500 응답</button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
