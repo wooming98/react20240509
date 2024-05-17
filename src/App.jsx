1ff1e1c8a35fa9861132676b199af103b5232109
anwoomin <askcw39@naver.com>
2024-05-17 15:14:22 +0900

    상위 경로 컴포넌트 추출, 스타일 줌

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
+function SpringRoot() {
+  return (
+    <div>
+      <div style={{ padding: "10x", backgroundColor: "#eee" }}>상단 NAVBAR</div>
+      <div style={{ marginTop: "20px" }}>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
 const router = createBrowserRouter([
   {
     path: "spring",
-    element: (
-      <div>
-        spring root page <Outlet />
-      </div>
-    ),
+    element: <SpringRoot />,
     children: [
       { path: "api", element: <div>api page</div> },
       { path: "doc", element: <div>doc page</div> },
