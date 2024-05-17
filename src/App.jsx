234cbd60522b9a8048cc9467fd34fa182c83cf12
anwoomin <askcw39@naver.com>
2024-05-17 12:16:58 +0900

    권한별 요청 경로

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import axios from "axios";
 
 function App(props) {
   const [username, setUsername] = useState("son");
+  const [scope, setScope] = useState("admin manager");
 
   function handleLogin() {
-    axios.post("/api/main44/login", { username }).then((res) => {
+    axios.post("/api/main44/login", { username, scope }).then((res) => {
       localStorage.setItem("token", res.data);
     });
   }
 
-  // 토큰 지우기
   function handleLogout() {
     localStorage.removeItem("token");
   }
@@ -29,22 +29,68 @@ function App(props) {
       .then((res) => alert(res.data));
   }
 
+  function handleAccessAdmin() {
+    axios
+      .get("/api/main44/admin", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => alert(res.data));
+  }
+
+  function handleAccessManager() {
+    axios
+      .get("/api/main44/manager", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => alert(res.data));
+  }
+
+  function handleAccessManagerOrAdmin() {
+    axios
+      .get("/api/main44/ma", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => alert(res.data));
+  }
+
   return (
     <div>
-      <input
-        type="text"
-        defaultValue={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
+      <div>
+        <input
+          type="text"
+          defaultValue={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          defaultValue={scope}
+          onChange={(e) => setScope(e.target.value)}
+        />
+      </div>
       <div>
         <button onClick={handleLogin}>로그인</button>
       </div>
+
       <hr />
       <button onClick={handleLogout}>로그아웃</button>
       <hr />
       <button onClick={handleAccessAll}>누구나</button>
       <hr />
       <button onClick={handleAccessUser}>로그인한 유저만</button>
+      <hr />
+      <button onClick={handleAccessAdmin}>어드민 경로</button>
+      <hr />
+      <button onClick={handleAccessManager}>매니져 경로</button>
+      <hr />
+      <button onClick={handleAccessManagerOrAdmin}>매니져/어드민 경로</button>
     </div>
   );
 }
