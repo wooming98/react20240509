dc6ec054ef69a952c1dd80bd6fd8a130751eec6a
anwoomin <askcw39@naver.com>
2024-05-17 14:19:03 +0900

    react router로 경로별 다른 컴포넌트 보여주기

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,24 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// router 객체
+const router = createBrowserRouter([
+  {
+    path: "/add",
+    element: <div>글 작성 페이지</div>,
+  },
+  {
+    path: "/edit",
+    element: <div>게시물 수정 페이지</div>,
+  },
+  {
+    path: "/list",
+    element: <div>게시물 목록 페이지</div>,
+  },
+]);
 
 function App(props) {
-  const [username, setUsername] = useState("son");
-  const [scope, setScope] = useState("admin manager");
-
-  function handleLogin() {
-    axios.post("/api/main44/login", { username, scope }).then((res) => {
-      localStorage.setItem("token", res.data);
-    });
-  }
-
-  function handleLogout() {
-    localStorage.removeItem("token");
-  }
-
-  function handleAccessAll() {
-    axios.get("/api/main44/all").then((res) => alert(res.data));
-  }
-
-  function handleAccessUser() {
-    axios
-      .get("/api/main44/user", {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      })
-      .then((res) => alert(res.data));
-  }
-
-  function handleAccessAdmin() {
-    axios
-      .get("/api/main44/admin", {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      })
-      .then((res) => alert(res.data));
-  }
-
-  function handleAccessManager() {
-    axios
-      .get("/api/main44/manager", {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      })
-      .then((res) => alert(res.data));
-  }
-
-  function handleAccessManagerOrAdmin() {
-    axios
-      .get("/api/main44/ma", {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      })
-      .then((res) => alert(res.data));
-  }
-
-  return (
-    <div>
-      <div>
-        <input
-          type="text"
-          defaultValue={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          defaultValue={scope}
-          onChange={(e) => setScope(e.target.value)}
-        />
-      </div>
-      <div>
-        <button onClick={handleLogin}>로그인</button>
-      </div>
-
-      <hr />
-      <button onClick={handleLogout}>로그아웃</button>
-      <hr />
-      <button onClick={handleAccessAll}>누구나</button>
-      <hr />
-      <button onClick={handleAccessUser}>로그인한 유저만</button>
-      <hr />
-      <button onClick={handleAccessAdmin}>어드민 경로</button>
-      <hr />
-      <button onClick={handleAccessManager}>매니져 경로</button>
-      <hr />
-      <button onClick={handleAccessManagerOrAdmin}>매니져/어드민 경로</button>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
