363b6709c9ca3f0081dc632555e024147e593ab8
anwoomin <askcw39@naver.com>
2024-05-17 16:01:23 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,7 @@
 import React from "react";
-import {
-  createBrowserRouter,
-  Link,
-  Outlet,
-  RouterProvider,
-} from "react-router-dom";
-
-function SpringRoot() {
-  return (
-    <div>
-      <div
-        style={{
-          padding: "10px",
-          backgroundColor: "#eee",
-          display: "flex",
-          gap: "5px",
-        }}
-      >
-        <div>
-          <Link to="/spring">HOME</Link>
-        </div>
-        <div>
-          {/* a 태그 대신 Link 컴포넌트 사용하기 */}
-          <Link to="/spring/learn">LEARN</Link>
-        </div>
-        <div>
-          <Link to="/spring/api">API</Link>
-        </div>
-        <div>
-          <Link to="/spring/doc">DOC</Link>
-        </div>
-      </div>
-
-      <div style={{ marginTop: "20px" }}>
-        <Outlet />
-      </div>
-    </div>
-  );
-}
-
-const router = createBrowserRouter([
-  {
-    path: "spring",
-    element: <SpringRoot />,
-    children: [
-      { index: true, element: <div>spring main page</div> },
-      { path: "api", element: <div>api page</div> },
-      { path: "doc", element: <div>doc page</div> },
-      { path: "learn", element: <div>learn page</div> },
-    ],
-  },
-]);
 
 function App(props) {
-  return <RouterProvider router={router} />;
+  return <div></div>;
 }
 
 export default App;
