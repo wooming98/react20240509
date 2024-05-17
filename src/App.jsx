10e46bf5bee94f2c7a0ffb65c3b6d7aad6e50e2f
anwoomin <askcw39@naver.com>
2024-05-17 16:17:07 +0900

    쿼리 스트링 읽기

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {
   Link,
   Outlet,
   RouterProvider,
+  useSearchParams,
 } from "react-router-dom";
 
 function Root() {
@@ -24,11 +25,24 @@ function Root() {
 function BoardList() {
   return (
     <div>
-      <div>1번 게시물 보기</div>
-      <div>2번 게시물 보기</div>
-      <div>3번 게시물 보기</div>
+      <div>
+        <Link to="/board?id=1">1번 게시물 보기</Link>
+      </div>
+      <div>
+        <Link to="/board?id=2">2번 게시물 보기</Link>
+      </div>
+      <div>
+        <Link to="/board?id=3">3번 게시물 보기</Link>
+      </div>
     </div>
   );
+}
+
+function BoardView() {
+  // /board?id=3
+  // query string 읽기
+  const [params] = useSearchParams();
+  return <div>{params.get("id")}번 게시물 보기</div>;
 }
 
 const router = createBrowserRouter([
@@ -42,7 +56,7 @@ const router = createBrowserRouter([
       },
       {
         path: "board",
-        element: <div>BOARD PAGE</div>,
+        element: <BoardView />,
       },
       {
         path: "list",
