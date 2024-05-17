dc45e3e33f1d80e26a6b5704408d87f05471eb7e
anwoomin <askcw39@naver.com>
2024-05-17 11:31:36 +0900

    로그인, 로그아웃
    누구나 접근 가능한 경로 요청
    로그인한 사용자만 접근 가능한 경로 요청

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  const [username, setUsername] = useState("son");
+
+  function handleLogin() {
+    axios.post("/api/main44/login", { username }).then((res) => {
+      localStorage.setItem("token", res.data);
+    });
+  }
+
+  // 토큰 지우기
+  function handleLogout() {
+    localStorage.removeItem("token");
+  }
+
+  function handleAccessAll() {
+    axios.get("/api/main44/all").then((res) => alert(res.data));
+  }
+
+  function handleAccessUser() {
+    axios
+      .get("/api/main44/user", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => alert(res.data));
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        defaultValue={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <div>
+        <button onClick={handleLogin}>로그인</button>
+      </div>
+      <hr />
+      <button onClick={handleLogout}>로그아웃</button>
+      <hr />
+      <button onClick={handleAccessAll}>누구나</button>
+      <hr />
+      <button onClick={handleAccessUser}>로그인한 유저만</button>
+    </div>
+  );
 }
 
 export default App;
