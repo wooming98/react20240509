5895373714c776396f7efb70a590fd4f967d9558
anwoomin <askcw39@naver.com>
2024-05-20 10:38:15 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <Button sx={{ color: "orange" }} colorScheme={"blue"}>
-        Lorem.
-      </Button>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
