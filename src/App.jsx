f077011fd0f5701070683d3c2669117e5cefe211
anwoomin <askcw39@naver.com>
2024-05-20 14:20:12 +0900

    이벤트 버블링 막기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Box, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  function handleButtonClick(e) {
+    // event bubbling 멈추기
+    e.stopPropagation();
+    console.log("버튼 클릭됨");
+  }
+
+  function handleBoxClick() {
+    console.log("박스 클릭됨");
+  }
+
+  return (
+    <ChakraProvider>
+      <Box onClick={handleBoxClick} w={"100px"} h={"100px"} bgColor={"orange"}>
+        <Button onClick={handleButtonClick}>클릭</Button>
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
