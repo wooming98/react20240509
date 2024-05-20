3177b5db60a1849719ecc7bc1ee5d882c03fb787
anwoomin <askcw39@naver.com>
2024-05-20 16:03:11 +0900

    axios로 파일 전송
    postForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App(props) {
+  const [name, setName] = useState("");
+  const [file, setFile] = useState(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    axios.postForm("/api/main45/sub1", {
+      name: name,
+      file: file,
+    });
+  }
+
   return (
     <div>
-      <form action="/path" method="POST" encType="multipart/form-data">
-        이름 <input type="text" name="name" />
+      <form onSubmit={handleSubmit}>
+        이름{" "}
+        <input
+          type="text"
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+        />
         <br />
-        <input type="file" name="file" />
+        <input
+          type="file"
+          name="file"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
         <br />
         <input type="submit" />
       </form>
