79e59a8969356234d0136b7f7c22d2116fd394b9
anwoomin <askcw39@naver.com>
2024-05-20 15:12:01 +0900

    file 전송

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <form action="/path" method="POST" encType="multipart/form-data">
+        이름 <input type="text" name="name" />
+        <br />
+        <input type="file" name="file" />
+        <br />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
