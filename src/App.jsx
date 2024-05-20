1f3f8206c1a5a163e54a940892ed54bad29a2de6
anwoomin <askcw39@naver.com>
2024-05-20 14:10:31 +0900

    input 요소 value에 값을 주고 변경하는 코드 작성할 것

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  const [text1, setText1] = useState("");
+  const [text2, setText2] = useState("hello");
+  const [text3, setText3] = useState("hi");
+  const [text4, setText4] = useState("안녕");
+  const [text5, setText5] = useState("빠이");
+
+  return (
+    <div>
+      <div>
+        <input type="text" onChange={(e) => setText1(e.target.value)} />
+      </div>
+      <div>
+        <input type="text" value={text2} readOnly={true} />
+      </div>
+      <div>
+        <input type="text" defaultValue={text3} />
+      </div>
+      <div>
+        <input
+          type="text"
+          value={text4}
+          onChange={(e) => setText4(e.target.value)}
+        />
+      </div>
+      <div>
+        <input type="text" value={text5} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
