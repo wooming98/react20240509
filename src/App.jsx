16b9c69b7a2dcb7474010026a4fd6b5e064ebad0
anwoomin <askcw39@naver.com>
2024-05-20 10:37:53 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 import React from "react";
-import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return (
-    <ChakraProvider>
-      <Box
-        bgColor={"orange"}
-        _hover={{
-          bgColor: "yellow",
-        }}
-      >
-        Lorem ipsum dolor.
-      </Box>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
