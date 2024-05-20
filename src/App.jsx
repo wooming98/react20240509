d6c477d57f2269636e6f12b8aa6e6c1db49a090e
anwoomin <askcw39@naver.com>
2024-05-20 10:17:42 +0900

    스페이스

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <Box m={1} bgColor={"tomato"}>
+        Lorem ipsum dolor.
+      </Box>
+      <Box m={2} bgColor={"tomato"}>
+        Lorem ipsum dolor.
+      </Box>
+      <Box m={10} bgColor={"tomato"}>
+        Possimus, rem tempore.
+      </Box>
+      <Box m={20} bgColor={"tomato"}>
+        Ex, fugit, odio.
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
