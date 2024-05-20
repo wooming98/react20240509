9d12533a4d3afa706cf442504b6420633c99f227
anwoomin <askcw39@naver.com>
2024-05-20 15:06:41 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,7 @@
 import React from "react";
 
 function App(props) {
-  function handleLinkClick(e) {
-    // 기본 이벤트 동작 막기
-    e.preventDefault();
-
-    // 개발자가 원하는 동작...
-  }
-
-  function handleSubmitForm(e) {
-    e.preventDefault();
-    console.log("다른 동작하는 코드 작성");
-  }
-
-  return (
-    <div>
-      <a href="https://www.naver.com" onClick={handleLinkClick}>
-        네이버
-      </a>
-      <hr />
-      <form action="https://www.naver.com" onSubmit={handleSubmitForm}>
-        <input type="text" />
-        <button>전송</button>
-      </form>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
