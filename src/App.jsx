a152d51b7355b163b13931b5e858285ea80a2a47
anwoomin <askcw39@naver.com>
2024-05-20 10:18:11 +0900

    반응형 스타일

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <Box bgColor={"tomato"} w={"500px"}>
+        Lorem ipsum dolor.
+      </Box>
+      <Box bgColor={"yellow.700"} w={{ base: "100%", md: "500px" }}>
+        Lorem ipsum dolor.
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
