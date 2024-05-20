7eae8cc60a9e839cc46ecd5e02cc848174736fc7
anwoomin <askcw39@naver.com>
2024-05-20 14:34:13 +0900

    초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
-import { Box, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  function handleButtonClick(e) {
-    // event bubbling 멈추기
-    e.stopPropagation();
-    console.log("버튼 클릭됨");
-  }
-
-  function handleBoxClick() {
-    console.log("박스 클릭됨");
-  }
-
-  return (
-    <ChakraProvider>
-      <Box onClick={handleBoxClick} w={"100px"} h={"100px"} bgColor={"orange"}>
-        <Button onClick={handleButtonClick}>클릭</Button>
-      </Box>
-    </ChakraProvider>
-  );
+  return <div></div>;
 }
 
 export default App;
