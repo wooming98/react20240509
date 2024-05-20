1e5f336c0c55cf2005f0e37d0c6a6b8689fe3e2c
anwoomin <askcw39@naver.com>
2024-05-20 11:20:16 +0900

    모달

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import {
+  Button,
+  ChakraProvider,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  return (
+    <ChakraProvider>
+      <Button onClick={onOpen}>모달 오픈!</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>TITLE</ModalHeader>
+          <ModalBody>Lorem ipsum dolor.</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>닫기</Button>
+            <Button colorScheme={"blue"}>저장</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </ChakraProvider>
+  );
 }
 
 export default App;
