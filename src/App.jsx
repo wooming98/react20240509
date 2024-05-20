c192505d86444996bf0c1cde4e5838fd2e2c7b5f
anwoomin <askcw39@naver.com>
2024-05-20 11:44:18 +0900

    chakra ui icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
+import { EmailIcon, StarIcon } from "@chakra-ui/icons";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <div>
+        <StarIcon />
+      </div>
+      <div>
+        <Button leftIcon={<EmailIcon />}>Email</Button>
+      </div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
