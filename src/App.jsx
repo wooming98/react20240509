0edbd67f3c49f69a178a4dc912cac8257501ea1b
anwoomin <askcw39@naver.com>
2024-05-20 10:38:06 +0900

    sx : 스타일 덮어쓰기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <Button sx={{ color: "orange" }} colorScheme={"blue"}>
+        Lorem.
+      </Button>
+    </ChakraProvider>
+  );
 }
 
 export default App;
